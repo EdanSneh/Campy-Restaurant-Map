dbd7623dd61b5ea327a0944512a632cfc63e4932
EdanSneh <edan.sneh@gmail.com>
2017-02-20 14:35:14 -0800

    implemented firsrt two resturants with basic template and js file works

---
+++ rest_info.js
@@ -0,0 +1,20 @@
+
+var markerobjects = [
+	//Pho Vi Hoa
+	{
+		"name"				: "Pho Vi Hoa",
+		"coordinate"		:[37.401108, -122.114195],
+		"site"				: "Campanile",
+		"img"				: "<img src='Images/pho.jpg' style='width:150px;height:110px;'>",
+		"link"				: "<a href='http://thecampanile.org/2014/01/27/vietnamese-food-pho-you/'>"
+	},
+	//Part 2
+	{
+		"name"				: "Pizza Studio",
+		"coordinate"		:[37.401878, -122.111827],
+		"site"				: "Verde",
+		"img"				: "<img src='Images/pizzastudio.jpg' style='width:150px;height:110px;'>",
+		"link"				: "<a href='http://verdemagazine.com/restaurant-review-pizza-studio'>"
+	}
+
+];

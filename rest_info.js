01d23449675c44f595d7d8051e944225ff3ef781
EdanSneh <edan.sneh@gmail.com>
2017-02-25 15:01:01 -0800

    10 resturants + fixed zoom

--- rest_info.js
+++ rest_info.js
@@ -8,13 +8,76 @@ var markerobjects = [
 		"img"				: "<img src='Images/pho.jpg' style='width:150px;height:110px;'>",
 		"link"				: "<a href='http://thecampanile.org/2014/01/27/vietnamese-food-pho-you/'>"
 	},
-	//Part 2
+	//Pizza Studio
 	{
 		"name"				: "Pizza Studio",
 		"coordinate"		:[37.401878, -122.111827],
 		"site"				: "Verde",
 		"img"				: "<img src='Images/pizzastudio.jpg' style='width:150px;height:110px;'>",
 		"link"				: "<a href='http://verdemagazine.com/restaurant-review-pizza-studio'>"
-	}
-
+	},
+	//Ada's Cafe
+	{
+		"name"				: "Ada's Cafe",
+		"coordinate"		:[37.422320, -122.112654],
+		"site"				: "C Mag",
+		"img"				: "<img src='Images/ada.jpg' style='width:110px;height:110px;'>",
+		"link"				: "<a href='http://cmagazine.org/445/food/add-a-story-to-your-site/'>"
+	},
+	//Zoe's Cafe
+	{
+		"name"				: "Cafe Zoe",
+		"coordinate"		:[37.459265, -122.152796],
+		"site"				: "C Mag",
+		"img"				: "<img src='Images/zoe.jpg' style='width:87px;height:108px;'>",
+		"link"				: "<a href='http://cmagazine.org/445/food/add-a-story-to-your-site/'>"
+	},
+	//Coupa Cafe
+	{
+		"name"				: "Coupa Cafe",
+		"coordinate"		:[37.444706, -122.161547],
+		"site"				: "C Mag",
+		"img"				: "<img src='Images/coupa.jpg' style='width:110px;height:110px;'>",
+		"link"				: "<a href='http://cmagazine.org/445/food/add-a-story-to-your-site/'>"
+	},
+	//Coho Cafe
+	{
+		"name"				: "Coho Cafe",
+		"coordinate"		:[37.424060, -122.170887],
+		"site"				: "C Mag",
+		"img"				: "<img src='Images/coho.jpg' style='width:110px;height:110px;'>",
+		"link"				: "<a href='http://cmagazine.org/445/food/add-a-story-to-your-site/'>"
+	},
+	//Kirk's
+	{
+		"name"				: "Kirk's Steakburgers",
+		"coordinate"		:[37.438116, -122.159442],
+		"site"				: "Verde",
+		"img"				: "<img src='Images/kirks.jpg' style='width:110px;height:110px;'>",
+		"link"				: "<a href='http://verdemagazine.com/pa-burgers-verde-reviews-popular-burgers-in-palo-alto'>"
+	},
+	//Umami Burger
+	{
+		"name"				: "Umami Burger",
+		"coordinate"		:[37.447623, -122.159577],
+		"site"				: "Verde",
+		"img"				: "<img src='Images/umami.jpg' style='width:250px;height:166px;'>",
+		"link"				: "<a href='http://verdemagazine.com/pa-burgers-verde-reviews-popular-burgers-in-palo-alto'>"
+	},
+	//Coho Cafe
+	{
+		"name"				: "Sliderbar",
+		"coordinate"		:[37.446150, -122.160982],
+		"site"				: "Verde",
+		"img"				: "<img src='Images/sliderbar.jpg' style='width:110px;height:110px;'>",
+		"link"				: "<a href='http://verdemagazine.com/pa-burgers-verde-reviews-popular-burgers-in-palo-alto'>"
+	},
+	//Freebirds
+	{
+		"name"				: "Freebirds",
+		"coordinate"		:[37.425825, -122.146631],
+		"site"				: "Campanile",
+		"img"				: "<img src='Images/freebirds.jpg' style='width:110px;height:110px;'>",
+		"link"				: "<a href='http://thecampanile.org/2013/02/22/new-mexican-restaurant-freebirds-combines-relaxed-rocker-atmosphere-with-freedom-to-customize-food/'>"
+	},
 ];
